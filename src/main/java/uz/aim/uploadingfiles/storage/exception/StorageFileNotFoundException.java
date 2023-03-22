8eb09d73de4e9cf4f4589e1474ad734f0df04814
aim030902 <aim030902@gmail.com>
2023-03-22 21:34:56 +0500

    first_commit

---
+++ src/main/java/uz/aim/uploadingfiles/storage/exception/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package uz.aim.uploadingfiles.storage.exception;
+
+public class StorageFileNotFoundException extends StorageException {
+
+	public StorageFileNotFoundException(String message) {
+		super(message);
+	}
+
+	public StorageFileNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
